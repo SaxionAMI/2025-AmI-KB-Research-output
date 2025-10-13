9c97ae32ab9cd426faecb9bfb7f49a30ce16d65d
Bram <b.t.ton@saxion.nl>
2025-10-13 09:58:00 +0200

    Add WO universities as well

--- scrapers/uasWikidata.py
+++ scrapers/uasWikidata.py
@@ -3,25 +3,26 @@ from SPARQLWrapper import SPARQLWrapper, XML
 wdEndPoint = 'https://query.wikidata.org/sparql'
 sparql = SPARQLWrapper(wdEndPoint)
 sparql.setQuery("""
-    PREFIX eurio: <http://data.europa.eu/s66#>
-    CONSTRUCT {
-      ?uas a eurio:Organisation .
-        ?uas rdfs:label ?name .
-          ?uas skos:altLabel ?altName .
-            ?uas eurio:hasSite ?webSite .
-            }
-            WHERE {
-            #{?uas wdt:P31 wd:Q38723 .} # is a higher education institution
-            #UNION
-            #{?uas wdt:P31 wd:Q17028020.} # is a vocational university
-              
-              ?uas wdt:P463 wd:Q2688654 . # member of Vereniging Hogescholen
-              ?uas rdfs:label ?name .
-              ?uas skos:altLabel ?altName .
-              OPTIONAL { ?uas wdt:P856 ?webSite . } # Official website
-              FILTER ( lang(?name) in ("en","nl") )
-              FILTER ( lang(?altName) in ("en","nl") )
-              }
+PREFIX eurio: <http://data.europa.eu/s66#>
+CONSTRUCT {
+  ?uas a eurio:Organisation .
+  ?uas eurio:legalName ?name .
+  ?uas skos:altLabel ?altName .
+  ?uas eurio:hasSite ?webSite .
+} WHERE {
+  {
+    ?uas wdt:P463 wd:Q2688654 . # member of Vereniging Hogescholen
+  } UNION {
+    ?uas wdt:P31 wd:Q3918 . # is a university
+    ?uas wdt:P17 wd:Q55 . # country Netherlands
+  }
+  OPTIONAL { ?uas wdt:P856 ?webSite . } # Official website
+  ?uas rdfs:label ?name .
+  ?uas skos:altLabel ?altName .
+  # Note: the mul is the default language
+  FILTER ( lang(?name) in ("en","nl", "mul") )
+  FILTER ( lang(?altName) in ("en","nl", "mul") )
+}
 """)
 sparql.setReturnFormat(XML)
 results = sparql.query().convert()
