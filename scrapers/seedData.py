60bad5c3e498f51767d1589bddd9c22f13d05fe2
Bram <b.t.ton@saxion.nl>
2025-10-17 21:39:06 +0200

    More seed data

--- scrapers/seedData.py
+++ scrapers/seedData.py
@@ -13,22 +13,31 @@ CONSTRUCT {
   {
     ?uas wdt:P463 wd:Q2688654 . # member of Vereniging Hogescholen
   } UNION {
-    ?uas wdt:P31 wd:Q3918 . # is a university
+    ?uas wdt:P31/wdt:P279* wd:Q3918 . # is a university (and the subclasses thereof)
     ?uas wdt:P17 wd:Q55 . # country Netherlands
   } UNION {
     ?uas wdt:P31 wd:Q31855 . # is a research institute
     ?uas wdt:P17 wd:Q55 . # country Netherlands
   } UNION {
-    BIND(wd:Q50037936 AS ?uas)
+    ?uas wdt:P31 wd:Q1059324 . # is a university hospital (de MC's)
+    ?uas wdt:P17 wd:Q55 . # country Netherlands
+  } UNION {
+    VALUES ?uas {wd:Q50037936 } . # SIA
+  } UNION {
+    VALUES ?uas {wd:Q131428044 } . # TFF
+  } UNION {
+    VALUES ?uas {wd:Q253439} . # KNAW
   }
-  OPTIONAL { ?uas wdt:P856 ?webSite . } # Official website
   ?uas rdfs:label ?name .
-  ?uas skos:altLabel ?altName .
+  OPTIONAL { ?uas wdt:P856 ?webSite . } # Official website
+  OPTIONAL {
+    ?uas skos:altLabel ?altName .
+    FILTER ( lang(?altName) in ("en","nl", "mul") )
+  }
   # Note: the mul is the default language
   FILTER ( lang(?name) in ("en","nl", "mul") )
-  FILTER ( lang(?altName) in ("en","nl", "mul") )
 }
 """)
 sparql.setReturnFormat(XML)
 results = sparql.query().convert()
-results.serialize(destination="uasWikidata.ttl")
+results.serialize(destination="seedData.ttl")
